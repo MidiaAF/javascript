d54941b7744f99288fd0cbf02eade2470c9c6677
Lorena200216 <midia.faustino@scania.com>
2023-09-25 07:51:43 -0300

    iniciando java

---
+++ cursos js/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+
+console.log(`A velocidade do seu carro é ${vel} KM/h`)
+if (vel >60)
+     {console.log(`Você ultrapassou a velocidade permitida. Multado!`)}
+console.log(`Dirija sempre usando cinto de segurança!`)
