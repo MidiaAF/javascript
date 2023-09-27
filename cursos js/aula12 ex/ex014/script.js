223197db9f9b704283e0d910ad5bbf697ff23be8
Lorena200216 <midia.faustino@scania.com>
2023-09-27 09:14:25 -0300

    atualizei ex015

--- cursos js/aula12 ex/ex014/script.js
+++ cursos js/aula12 ex/ex014/script.js
@@ -4,16 +4,24 @@ var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem') 
 var data = new Date()
 var hora = data.getHours()
+
+
+
 msg.innerHTML = `Agora são ${hora} horas.`
+
 if(hora >=0 && hora < 12){
    //Bom dia
    img.src = 'manha.png'
-    } else if (hora >= 12 && hora < 18){
+   document.body.style.background = '#d0c267bd' 
+   
+    } else if (hora >= 12 && hora <= 18){
    //Boa tarde
     img.src = 'tarde.png'
+    document.body.style.background = '#d19169'
     }else {
         //Boa noite
         img.src = 'noite.png'
+        document.body.style.background = '#7388ab'
     }
 }
 
