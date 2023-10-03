23d8adeb61f3bfe1632afa45b14691ce3ab7d8a0
Lorena200216 <midia.faustino@scania.com>
2023-10-03 08:05:35 -0300

    criei ex016

--- aula14ex/ex016.contador/script.js
+++ aula14ex/ex016.contador/script.js
@@ -1,12 +1,28 @@
 function contar() {
-    var ini = document.getElementById('txti').value
-    var fim = document.getElementById('txtf').value    
-    var passo = document.getElementById ('txtp').value
+    let ini = document.getElementById('txti')//caixa de texto ainda não é numero 
+    let fim = document.getElementById('txtf') 
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res') 
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert(`[Erro] Faltam dados`)
-    } else if {
-        alert(`Tudo OK`)
+    } else {
+      res.innerHTML = 'Contando...'
+      let i = Number(ini.value)//transformando cx de texto em numero
+      let f = Number(fim.value)
+      let p = Number(passo.value)// soma contando
+
+      if(i < f) { 
+        for (let c = i; c <= f; c += p){
+        res.innerHTML += `${c} \u{1F449}` // seta 
+      }      
+     
+      res.innerHTML += `\u{1f3c1}`// bandeira 
+       } else { 
+
+       }
+
     }
     
 }
