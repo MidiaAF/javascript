1fe996088d9e5870db6ed67660775f99ca2eaa7e
Lorena200216 <midia.faustino@scania.com>
2023-10-10 15:11:07 -0300

    excecutando ex

--- exercicio.final/script.js
+++ exercicio.final/script.js
@@ -1,12 +1,19 @@
-function adicionar (){
-let numero = document.getElementById('num')
-let finalizar = document.getElementById ('fin')
-let resultado = document.getElementById('res')
 
-if(num.value.length == 0 || finalizar.value.length ==0 ){
-    window.alert( 'Valor inválido!')
+let num = document.getElementById('dignum')
+let inf = document.getElementById ('fin')
+let res = document.getElementById('res')
+let valores = []
+let res = document.getElementById('res')
+
+function adicionar(){
+    
+if(num.value.length == 0 ||inf.value.length == 0 ||  ){
+    window.alert( 'Valor inválido ou já encontrado na lista!')
 } else {
     let n = Number(num.value)
+
+
+    for(let) 
 
 }
 } 
